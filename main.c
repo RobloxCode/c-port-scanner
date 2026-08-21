551e55893c275db2dc95d92d4b9cb8541de26757
Miguel Loranca <mloranca703@gmail.com>
2026-08-20 19:40:06 -0600

    added basic function

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
+#include <arpa/inet.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+bool is_connected(int port) {
+    int sockfd;
+    struct sockaddr_in addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return false;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        close(sockfd);
+        return true;
+    }
+
+    close(sockfd);
+    return false;
+}
 
 int main(void) {
-    printf("tehuoet");
+    int port = 80;
+    if (is_connected(port)) {
+        printf("computer connected to port: %d\n", port);
+    } else {
+        printf("computer not connected to port: %d\n", port);
+    }
+
     return 0;
 }
